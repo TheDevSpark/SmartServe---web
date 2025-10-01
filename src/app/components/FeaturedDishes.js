5c4e2fcc07efc6d79a01bd48ffb9ffcdd8ea2417
Ambar Naz <ambarnaz11@gmail.com>
2025-10-02 03:00:18 +0500

    feedback and digital menu page done

--- src/app/components/FeaturedDishes.js
+++ src/app/components/FeaturedDishes.js
@@ -71,7 +71,7 @@ const FeaturedDishes = () => {
         {/* Featured Dishes Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
           {featuredDishes.map((dish) => (
-            <DishCard key={dish.id} dish={dish} />
+            <DishCard key={dish.id} dish={dish} showCategory={true} />
           ))}
         </div>
 
